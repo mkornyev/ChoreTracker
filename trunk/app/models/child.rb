92e71c26ea9cc604f4c0810bd1d94d547c98f8b7
mkornyev <mkornyev@gmail.com>
2019-02-14 21:04:56 -0500

    Got 6% test coverage, no model errors, +1 Chores error

--- trunk/app/models/child.rb
+++ trunk/app/models/child.rb
@@ -1,7 +1,17 @@
 class Child < ApplicationRecord
+	#byebug
 	has_many :chores
+	has_many :tasks, through: :chores
 
 
-	validates_presence_of :first_name, :last_name
-	
+	validates_presence_of :first_name
+	validates_presence_of :last_name
+
+	def name
+  		return self.first_name.to_s + " " + self.last_name.to_s
+	end
+
+	scope :alphabetical, -> { order('last_name', 'first_name') }
+	scope :active, -> { where(active: true) }
+
 end
